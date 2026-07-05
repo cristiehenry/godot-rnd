ab5ea647cf824b36cb1658ef55a37504ab48408a
Cristie Henry <cristiehenry@Cristies-MacBook-Pro.local>
2026-07-05 16:38:05 -0700

    initial commit

---
+++ scenes/control.gd
@@ -0,0 +1,24 @@
+extends Control
+
+@onready var enter_here=$Panel/EnterHere
+@onready var success_message=$Panel/SuccessMessage
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+
+func _on_submit_button_pressed() -> void:
+	var text = enter_here.text
+	text = text.strip_edges().to_lower()
+	
+	if text == "hello":
+		success_message.text = "You WIN"
+	else:
+		success_message.text = "try again"
+		
